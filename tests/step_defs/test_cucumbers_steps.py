93cd95d5e4fb28040e5158a0ef3bea7dd7b5f44a
Matthew Dolan <mdolancode@gmail.com>
2021-10-12 16:45:32 -0700

    Parametrize steps, and use partial functions.

--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -1,14 +1,23 @@
-from pytest_bdd import scenario, parsers, given, when, then
+from functools import partial
+from pytest_bdd import scenarios, parsers, given, when, then
 
 from cucumbers import CucumberBasket
 
-@scenario('../features/cucumbers.feature', 'Add cucumbers to a basket')
-def test_add():
-    pass
+scenarios('../features/cucumbers.feature')
 
-@scenario('../features/cucumbers.feature', 'Remove cucumbers from a basket')
-def test_remove():
-    pass
+EXTRA_TYPES = {
+    'Number': int,
+}
+
+parse_num = partial(parsers.cfparse, extra_types=EXTRA_TYPES)
+
+# @scenario('../features/cucumbers.feature', 'Add cucumbers to a basket')
+# def test_add():
+#     pass
+
+# @scenario('../features/cucumbers.feature', 'Remove cucumbers from a basket')
+# def test_remove():
+#     pass
 
 # This function below is a pytest fixture.
 # It's used by all of the other step definition functions as a fixture.
@@ -17,18 +26,18 @@ def test_remove():
 
 # Add parsers
 
-@given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=dict(Number=int)), target_fixture='basket')
+@given(parse_num('the basket has "{initial:Number}" cucumbers'), target_fixture='basket')
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
-@when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=dict(Number=int)))
+@when(parse_num('"{some:Number}" cucumbers are added to the basket'))
 def add_cucumbers(basket, some):
     basket.add(some)
 
-@when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=dict(Number=int)))
+@when(parse_num('"{some:Number}" cucumbers are removed from the basket'))
 def remove_cucumbers(basket, some):
     basket.remove(some)
 
-@then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=dict(Number=int)))
+@then(parse_num('the basket contains "{total:Number}" cucumbers'))
 def basket_has_total(basket, total):
     assert basket.count == total
